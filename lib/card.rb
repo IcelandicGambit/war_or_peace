2d082454abd91db690936943810b27df0235c2ca
Andrew Smith <andrewpatrick138@gmail.com>
2021-02-07 14:05:45 -0800

    remove comments

--- lib/card.rb
+++ lib/card.rb
@@ -4,21 +4,19 @@ class Card
     @value = value
     @rank = rank
   end
+
   def suit
     @suit
   end
+
   def value
     @value
   end
+
   def rank
     @rank
   end
 
-
-  #I had to write this to get assert_equals in test_pile_cards to work correctly
-   # Basically this method defines how to tell if two Card objects are equal (they
-   # have the same suit, value and rank) which is
-   # what is implicitly used in assert_equal [card1,card3], turn.spoils_of_war (line 91 of turn_test.rb)
    def ==(other)
      if other.nil? || !other.instance_of?(Card)
        false
--- lib/player.rb
+++ lib/player.rb
@@ -16,8 +16,7 @@ class Player
     @deck.cards.empty?
   end
 
-  #read the comment on this method in card.rb
-  # I need to test if to Player objects are equal.
+  
   def ==(other)
     if other.nil? || !other.instance_of?(Player)
       false
